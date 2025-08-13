1b923b975f2c31f0b18130f653716d21bf757e80
cuixin <xin.cui@ninebor.com>
2025-08-13 14:22:23 +0800

    [feat](项目初始化): 左侧添加收缩按钮

---
+++ src/views/home/cmponents/headerContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="header">
+		<p>欢迎来到首页</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+	height: 50px;
+}
+</style>
