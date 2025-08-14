eaa6c1dfd662f3faf07c5a4766b99292f424dd4e
cuixin <xin.cui@ninebor.com>
2025-08-14 15:12:41 +0800

    [feat](项目配置): monorepo架构|增加亮暗色切换按钮

---
+++ packages/blocks/src/ninebotBlock.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>标题</div>
+</template>
+
+<script setup lang="ts">
+type NinebotBlock = {
+	type: 'ninebot',
+	props:{
+		id: string,
+		name: string,
+	},
+	style: {
+		backgroundColor: string,
+		border: string,
+	},
+	events: {
+		onClick: () => void,
+	},
+}
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/home/cmponents/headerContainer.vue
+++ src/views/home/cmponents/headerContainer.vue
@@ -1,15 +1,43 @@
 <template>
   <div class="header">
 		<p>欢迎来到首页</p>
+		<div class="theme-switch">
+			<el-switch
+				v-model="themeColor"
+				:active-action-icon="Moon"
+        :inactive-action-icon="Sunny"
+				@change="handleThemeChange"
+			/>
+		</div>
+		
 	</div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { Sunny, Moon } from '@element-plus/icons-vue'
+
+const themeColor = ref(false)
+
+function handleThemeChange(value: boolean) {
+  console.log('主题切换:', value ? '暗黑模式' : '亮色模式')
+}
 
 </script>
 
 <style lang="scss" scoped>
 .header {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
 	height: 50px;
+	background-color: #fff;
+}
+.theme-switch {
+	margin-right: 20px;
+}
+.el-switch {
+	--el-switch-on-color: #2c2c2c;
+	--el-switch-off-color: #f2f2f2;
 }
 </style>
